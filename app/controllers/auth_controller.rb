14f98be333827e99911587424b40c6a020facdb7
Thomas <yitunghuang83@gmail.com>
2020-06-06 15:01:56 +0930

    Add Auth Controller

---
+++ app/controllers/auth_controller.rb
@@ -0,0 +1,12 @@
+class AuthController < ApplicationController
+  def login
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      payload = {user_id: user.id}
+      token = encode_token(payload)
+      render json: {user: user, jwt: token, success: "Welcome back, #{user.username}"}
+    else
+      render json: {failure: "Sorry, the login details you have entered is invalid."}
+    end
+  end
+end
