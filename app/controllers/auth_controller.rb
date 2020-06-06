e156e1f3559eb4731cfaf5d92f22e352e141b77f
Thomas <yitunghuang83@gmail.com>
2020-06-06 15:48:42 +0930

    Authenticate user logins and returns JWT

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,10 +1,11 @@
 class AuthController < ApplicationController
   def login
-    user = User.find_by(username: params[:username])
-    if user && user.authenticate(params[:password])
+    user = User.find_by(username: params[:auth][:username])
+
+    if user && user.authenticate(params[:auth][:password])
       payload = {user_id: user.id}
       token = encode_token(payload)
-      render json: {user: user, jwt: token, success: "Welcome back, #{user.username}"}
+      render json: {jwt: token, success: "Welcome back, #{user.username}"}
     else
       render json: {failure: "Sorry, the login details you have entered is invalid."}
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   root 'pages#login'
-  # 
-  # post "/login", to: "auth#login"
-  #
-  # resources :users, only: [:create]
 
+  post "/", to: "auth#login"
+
+  resources :users, only: [:create]
   resources :customers
 
 end
