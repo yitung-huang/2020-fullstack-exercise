f10e090c8b81ca2ab11c738b00997fb3bfbca3cd
Thomas <yitunghuang83@gmail.com>
2020-06-08 11:54:31 +0930

    Fix tags display

--- app/javascript/bundles/Customer/components/ResourceFilterDisplay.jsx
+++ app/javascript/bundles/Customer/components/ResourceFilterDisplay.jsx
@@ -1,6 +1,14 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
+/* Not sure why string arrays are passed as a string,
+ * so this is a helper function to convert the string
+ * back to an array. */
+let stringToArray = (string) => {
+  string = string.replace(/[\[\]\"]/g, "");
+  return string.split(", ");
+}
+
 export default class ResourceFilterDisplay extends React.Component {
   static propTypes = {
     name: PropTypes.string.isRequired, // this is passed from the Rails view
@@ -40,11 +48,17 @@ export default class ResourceFilterDisplay extends React.Component {
           </tr>
           {
             this.props.resources.map(function(resource){
+              let tag_string = "";
+              let resource_array = stringToArray(resource.tags);
+              console.log(resource_array);
+              for (let i = 0; i < resource_array.length; i++){
+                tag_string += resource_array[i] + " ";
+              }
               return (
                 <tr>
                   <td>{resource.name}</td>
                   <td>{resource.num_employees}</td>
-                  <td>{resource.tags.toString()}</td>
+                  <td>{tag_string}</td>
                 </tr>
               )
             })
