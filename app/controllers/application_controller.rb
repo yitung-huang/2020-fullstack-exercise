bd5a2a37bc2d2e82d189792f7ae8626afe3a38b6
Thomas <yitunghuang83@gmail.com>
2020-06-08 17:31:55 +0930

    fixup! Add authentication checks to customer list

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :logged_in?
 
   def current_user
-    if !defined?(auth)
-      auth = {}
-      auth[:user_id] = nil
-    end
-    @current_user ||= @current_user = User.find(auth[:user_id]) if auth && auth[:user_id]
+    @current_user ||= @current_user = User.find(session[:user_id]) if session[:user_id]
   end
 
   def logged_in?
